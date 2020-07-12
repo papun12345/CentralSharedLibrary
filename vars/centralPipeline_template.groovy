efce01b8e5dd0becee50772f6ee209ecce0f0a72
papun12345 <50276904+papun12345@users.noreply.github.com>
2020-07-12 16:21:42 +0530

    Update centralPipeline_template.groovy

--- vars/centralPipeline_template.groovy
+++ vars/centralPipeline_template.groovy
@@ -1,39 +1,32 @@
-pipelineJob('SomeTest')  {
-        definition {
-        cps {
-            script('''
-                        pipeline {
-                                    agent any
-                                    stages {
-                                        stage('CleanUp'){
-                                            steps{
-                                                deleteDir()
-                                            }
-                                        }
-                                        stage('Check-Out'){
-                                            steps{
-                                                 checkOutFrom('http://nitin@ec2-18-224-68-30.us-east-2.compute.amazonaws.com:7990/scm/sub/maven-project-sample.git','wipro-bitbucket')
-                                            }
-                                        }
-                                        stage('Run-Build-Inside-container'){
-                                            steps{
-                                                         dockerScript()
-                                            }
-                                        }
-                                        stage('Sonar-Scanning'){
-                                            steps{
-                                                sonarScanning()
-                                            }
-                                        }
-                                        stage('CodeCovrage-Check'){
-                                            steps{
-                                                codeCovrageCheck()
-                                            }
-                                        }
-                                    }
-                                }
-                         '''.stripIndent())
-            sandbox()
+def CreatePipeline(){
+        pipeline {
+            agent any
+            stages {
+                stage('CleanUp'){
+                    steps{
+                        deleteDir()
+                    }
+                }
+                stage('Check-Out'){
+                    steps{
+                         checkOutFrom('http://nitin@ec2-18-224-68-30.us-east-2.compute.amazonaws.com:7990/scm/sub/maven-project-sample.git','wipro-bitbucket')
+                    }
+                }
+                stage('Run-Build-Inside-container'){
+                    steps{
+                                 dockerScript()
+                    }
+                }
+                stage('Sonar-Scanning'){
+                    steps{
+                        sonarScanning()
+                    }
+                }
+                stage('CodeCovrage-Check'){
+                    steps{
+                        codeCovrageCheck()
+                    }
+                }
+            }
         }
     }
-}
