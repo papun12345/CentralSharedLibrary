fb891d722b866ad93da4c7eed851f2a949a7aeed
papun12345 <50276904+papun12345@users.noreply.github.com>
2020-07-12 21:28:46 +0530

    Update dockerScript.groovy

--- vars/dockerScript.groovy
+++ vars/dockerScript.groovy
@@ -2,11 +2,11 @@
 def call() {
   echo "************************************Run Build Inside Container**************************************"
   echo "This is invoking with $dockerImage $mavenCommand"
-  //def dockerImage=docker
-  //def mavenCommand=command
- // docker.image(dockerImage).inside(){
-//	sh mavenCommand
-//}
+  def dockerImage=docker
+  def mavenCommand=command
+  docker.image($dockerImage).inside(){
+	  sh $mavenCommand
+}
 }
 //"subhasanket/java-maven-node-env"
 //"mvn clean package"
