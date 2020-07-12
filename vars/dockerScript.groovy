fb077ca55e3ecbebe594c35b88e68b51770599b9
papun12345 <50276904+papun12345@users.noreply.github.com>
2020-07-12 21:17:19 +0530

    Update dockerScript.groovy

--- vars/dockerScript.groovy
+++ vars/dockerScript.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
-def call(docker,command) {
+def call() {
   echo "************************************Run Build Inside Container**************************************"
-  def dockerImage=docker
-  def mavenCommand=command
-  docker.image(dockerImage).inside(){
-	sh mavenCommand
+  echo "This is invoking with $dockerImage $mavenCommand"
+  //def dockerImage=docker
+  //def mavenCommand=command
+ // docker.image(dockerImage).inside(){
+//	sh mavenCommand
 }
 }
 //"subhasanket/java-maven-node-env"
