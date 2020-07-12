6ab7dde415f5a484455ee057bd6271339f71b0a0
papun12345 <50276904+papun12345@users.noreply.github.com>
2020-07-12 19:47:49 +0530

    Update checkOutFrom.groovy

--- vars/checkOutFrom.groovy
+++ vars/checkOutFrom.groovy
@@ -11,6 +11,4 @@ def call(url,credentialId) {
             submoduleCfg: [], 
             userRemoteConfigs: [[credentialsId: scmCredential, url: scmUrl]]
   ])
-  //'http://nitin@ec2-18-224-68-30.us-east-2.compute.amazonaws.com:7990/scm/sub/maven-project-sample.git'
-  //'wipro-bitbucket'
 }
