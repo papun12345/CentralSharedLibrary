32298651e8790912903e1f84c9516e084957fcf0
papun12345 <50276904+papun12345@users.noreply.github.com>
2020-07-12 20:00:27 +0530

    Update dockerScript.groovy

--- vars/dockerScript.groovy
+++ vars/dockerScript.groovy
@@ -1,5 +1,4 @@
 #!/usr/bin/env groovy
-
 def call(dockerImage,mavenCommand) {
   echo "************************************Run Build Inside Container**************************************"
   def dockerImage=dockerImage
