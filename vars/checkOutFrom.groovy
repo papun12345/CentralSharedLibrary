ce32894dad9ad5b78d5b43626444c0ea0d503142
papun12345 <50276904+papun12345@users.noreply.github.com>
2020-07-13 08:55:24 +0530

    Update checkOutFrom.groovy

--- vars/checkOutFrom.groovy
+++ vars/checkOutFrom.groovy
@@ -1,14 +1,12 @@
 #!/usr/bin/groovy
-def call(url,credentialId) {
+def call() {
   echo "************************************CheckOut**************************************"
-  def scmUrl=url
-  def scmCredential=credentialId
   checkout([
     		$class: 'GitSCM', 
             branches: [[name: '*/master']],
             doGenerateSubmoduleConfigurations: false,
             extensions: [[$class: 'CleanBeforeCheckout']],
             submoduleCfg: [], 
-            userRemoteConfigs: [[credentialsId: scmCredential, url: scmUrl]]
+            userRemoteConfigs: [[credentialsId: "${env.scmCredential}", url: "${env.scmUrl}"]]
   ])
 }
